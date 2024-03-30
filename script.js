74242662df6c52a0c810d56e9f93e14d38e2ff21
Madhushika Dissanayake <120777135+madhushika99@users.noreply.github.com>
2024-03-30 14:19:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+
+function generate() {
+    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.ariaValueMax;
+        imgBox.classList.add("show-img");
+}
+
